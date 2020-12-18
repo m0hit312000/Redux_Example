d8f292ea41a529d4087717a510bb81749ff1d25f
Mohit <mm8085574989@gmail.com>
2020-12-18 19:18:38 +0530

    redux example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Post from './components/post';
+import PostForm from './components/postForm';
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './store';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <PostForm />
+          <hr />
+          <Post />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
